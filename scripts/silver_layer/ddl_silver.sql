bef16e3291dc88c08a0b3a3664196101d5ceacfc
Tosin Ogundana <118941392+TosinOgundana@users.noreply.github.com>
2025-02-18 18:30:14 +0000

    Update ddl_silver.sql

--- scripts/silver_layer/ddl_silver.sql
+++ scripts/silver_layer/ddl_silver.sql
@@ -20,7 +20,8 @@ CREATE TABLE silver.crm_prd_info(
 	prd_cost INT,
 	prd_line NVARCHAR(50),
 	prd_start_dt DATE,
-	prd_end_dt DATE
+	prd_end_dt DATE,
+	dwh_create_date DATETTIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
@@ -29,12 +30,13 @@ CREATE TABLE silver.crm_sales_details (
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_shipt_dt INT,
-	sls_due_dt INT,
+	sls_order_dt DATE,
+	sls_shipt_dt DATE,
+	sls_due_dt DATE,
 	sls_sales INT,
 	sls_quantity INT,
-	sls_price INT
+	sls_price INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID ('silver.erp_cust_az12', 'U') IS NOT NULL
