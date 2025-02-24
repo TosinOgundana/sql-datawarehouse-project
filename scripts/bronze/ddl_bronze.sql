fb82ca7428cb061d37f03fd5443b5a10118347c7
Tosin Ogundana <118941392+TosinOgundana@users.noreply.github.com>
2025-02-24 16:30:22 +0000

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -18,7 +18,8 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_gndr NVARCHAR(50),
 	cst_create_date DATETIME
 );
-
+GO
+	
 IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info(
@@ -30,7 +31,8 @@ CREATE TABLE bronze.crm_prd_info(
 	prd_start_dt DATETIME,
 	prd_end_dt DATETIME
 );
-
+GO
+	
 IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
@@ -44,7 +46,8 @@ CREATE TABLE bronze.crm_sales_details (
 	sls_quantity INT,
 	sls_price INT
 );
-
+GO
+	
 IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12(
@@ -52,14 +55,16 @@ CREATE TABLE bronze.erp_cust_az12(
 	bdate DATE,
 	gen NVARCHAR (50)
 );
-
+GO
+	
 IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101(
 	cid NVARCHAR (50),
 	cntry NVARCHAR (50)
 );
-
+GO
+	
 IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
 CREATE TABLE bronze.erp_px_cat_g1v2(
@@ -68,3 +73,4 @@ CREATE TABLE bronze.erp_px_cat_g1v2(
 	subcat NVARCHAR (50),
 	maintenance NVARCHAR (50)
 );
+GO
