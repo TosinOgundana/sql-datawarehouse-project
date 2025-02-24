849f6174d1c1bb03a78b8950d20a326b12f98881
Tosin Ogundana <118941392+TosinOgundana@users.noreply.github.com>
2025-02-24 16:37:10 +0000

    Update ddl_silver.sql

--- scripts/silver_layer/ddl_silver.sql
+++ scripts/silver_layer/ddl_silver.sql
@@ -9,6 +9,7 @@ This script re-defines the DDL structure of 'bronze' tables when run.
 
 IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info;
+GO
 CREATE TABLE silver.crm_cust_info (
 	cst_id 		   INT,
 	cst_key 	   NVARCHAR(50),
@@ -23,6 +24,7 @@ GO
 	
 IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
+GO
 CREATE TABLE silver.crm_prd_info(
 	prd_id       INT,
 	cat_id       NVARCHAR(50),
@@ -38,6 +40,7 @@ GO
 	
 IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details;
+GO
 CREATE TABLE silver.crm_sales_details (
 	sls_ord_num   NVARCHAR(50),
 	sls_prd_key   NVARCHAR(50),
@@ -54,6 +57,7 @@ GO
 	
 IF OBJECT_ID ('silver.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12;
+GO
 CREATE TABLE silver.erp_cust_az12(
 	cid   NVARCHAR(50),
 	bdate DATE,
@@ -64,6 +68,7 @@ GO
 	
 IF OBJECT_ID ('silver.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101;
+GO
 CREATE TABLE silver.erp_loc_a101(
 	cid   NVARCHAR (50),
 	cntry NVARCHAR (50),
@@ -73,6 +78,7 @@ GO
 	
 IF OBJECT_ID ('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE silver.erp_px_cat_g1v2;
+GO
 CREATE TABLE silver.erp_px_cat_g1v2(
 	id     NVARCHAR (50),
 	cat    NVARCHAR (50),
